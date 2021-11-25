4d1882633a097d270c52aaa1683831fad916fc93
iourivolkov <volk.iouri@gmail.com>
2021-11-25 17:41:42 +0000

    update students.js to use parametrized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -13,15 +13,25 @@ const pool = new Pool({
 // start querying the database
 // pool query is a function that accepts an SQL query as a JS string - using backticks can write multi-line string
 
-// write query that returns a promise containing result
-pool.query(`
+// write parametrized queries to prevent SQL injection attacks
+
+
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
 
+// write query that returns a promise containing result
+pool
+.query(queryString, values)
 // if promise resolves --> print the response 
 .then(res => {
   // prints rows --> array of JS objects 
