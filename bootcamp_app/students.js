46ced7e72b14f53ae3c9d6bc5988d2f1b5a52ba5
iourivolkov <volk.iouri@gmail.com>
2021-11-25 15:04:54 +0000

    add queries for students and teachers in node

---
+++ bootcamp_app/students.js
@@ -0,0 +1,44 @@
+const { Pool } = require('pg');
+
+// set up connection to the bootcampx database (will need to store pw using env vars later)
+// client <--> pool - either can be used but pool is preferred 
+// pool will manage multiple clinet connections for us 
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// start querying the database
+// pool query is a function that accepts an SQL query as a JS string - using backticks can write multi-line string
+
+// write query that returns a promise containing result
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+
+// if promise resolves --> print the response 
+.then(res => {
+  // prints rows --> array of JS objects 
+  // use template literals to create a more powerful query
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`); 
+  })
+})
+// if promise fails (error) --> print error
+.catch(err => console.error('query error', err.stack));
+
+// output produces an object 
+// rows property contains array of expected results 
+
+// JS produces same data as SQL query h/e JS output givces us JS objects 
+
+
+
+
+
