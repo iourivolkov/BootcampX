a327271ac2720ca9b68c6399e1142d663dc838a3
iourivolkov <volk.iouri@gmail.com>
2021-11-25 18:05:10 +0000

    update teachers.js to use parametrized queries

---
+++ teachers.js
@@ -0,0 +1,33 @@
+const { Pool } = require('pg');
+
+// set up connection to the bootcampx database (will need to store pw using env vars later)
+// client <--> pool - either can be used but pool is preferred 
+// pool will manage multiple clinet connections for us 
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2] || 'JUL02';
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
+
+pool
+.query(queryString, [cohortName])
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
+
+
